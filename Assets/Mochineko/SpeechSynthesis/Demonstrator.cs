fe93c61d724b5d8197cb9adbe2895ca5d908938d
mochi-neko <t.o.e.4315@gmail.com>
2023-05-26 16:42:37 +0900

    Add direct sound option

--- Assets/Mochineko/SpeechSynthesis/Demonstrator.cs
+++ Assets/Mochineko/SpeechSynthesis/Demonstrator.cs
@@ -1,6 +1,8 @@
 #nullable enable
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using Cysharp.Threading.Tasks;
@@ -10,6 +12,7 @@ using Mochineko.SimpleAudioCodec;
 using Mochineko.VOICEVOX_API;
 using Mochineko.VOICEVOX_API.QueryCreation;
 using Mochineko.VOICEVOX_API.Synthesis;
+using NAudio.Wave;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -22,6 +25,9 @@ namespace Mochineko.SpeechSynthesis
 
         [SerializeField]
         private SynthesisBackend synthesisBackend = SynthesisBackend.VoiceVox;
+
+        [SerializeField]
+        private PlayerBackend playerBackend = PlayerBackend.AudioSource;
 
         [SerializeField]
         private string voicevoxBaseURL = "http://127.0.0.1:50021";
@@ -41,13 +47,25 @@ namespace Mochineko.SpeechSynthesis
         [SerializeField]
         private AudioSource? audioSource = default;
 
+        [SerializeField]
+        private int directSoundDeviceID = 0;
+
+        [SerializeField]
+        private List<DirectSoundDeviceInfo> directSoundDevices = new();
+
         private static readonly HttpClient HttpClient = new();
 
         private AudioClip? audioClip = default;
+        private DirectSoundOut? directSoundDevice = default;
 
         private void Awake()
         {
             Assert.IsNotNull(audioSource);
+        }
+
+        private void Start()
+        {
+            UpdateDevices();
         }
 
         private void OnDestroy()
@@ -56,18 +74,82 @@ namespace Mochineko.SpeechSynthesis
             {
                 UnityEngine.Object.Destroy(audioClip);
             }
+
+            directSoundDevice?.Dispose();
         }
 
-        private async UniTask SynthesisSpeechAsync(CancellationToken cancellationToken)
+        [ContextMenu(nameof(Synthesize))]
+        public void Synthesize()
+        {
+            SynthesizeSpeechAsync(this.GetCancellationTokenOnDestroy())
+                .Forget();
+        }
+
+        [ContextMenu(nameof(Replay))]
+        public void Replay()
+        {
+            switch (playerBackend)
+            {
+                case PlayerBackend.AudioSource:
+                    ReplayByAudioSource();
+                    break;
+
+                case PlayerBackend.DirectSound:
+                    ReplayByDirectSound();
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(playerBackend));
+            }
+        }
+
+        private void ReplayByAudioSource()
+        {
+            if (audioSource == null)
+            {
+                throw new NullReferenceException(nameof(audioSource));
+            }
+
+            if (audioClip == null)
+            {
+                Debug.Log($"[SpeechSynthesis] No audio clip to replay.");
+                return;
+            }
+
+            audioSource
+                .PlayAsync(audioClip, this.GetCancellationTokenOnDestroy())
+                .Forget();
+        }
+
+        private void ReplayByDirectSound()
+        {
+            if (directSoundDevice == null)
+            {
+                Debug.Log($"[SpeechSynthesis] No direct sound device to replay.");
+                return;
+            }
+
+            directSoundDevice
+                .PlayAsync(this.GetCancellationTokenOnDestroy())
+                .Forget();
+        }
+
+        [ContextMenu(nameof(UpdateDevices))]
+        public void UpdateDevices()
+        {
+            directSoundDevices = DirectSoundOut.Devices.ToList();
+        }
+
+        private async UniTask SynthesizeSpeechAsync(CancellationToken cancellationToken)
         {
             switch (synthesisBackend)
             {
                 case SynthesisBackend.VoiceVox:
-                    await SynthesisSpeechByVoicevoxAsync(cancellationToken);
+                    await SynthesizeSpeechByVoicevoxAsync(cancellationToken);
                     break;
 
                 case SynthesisBackend.Koeiromap:
-                    await SynthesisSpeechByKoeiromapAsync(cancellationToken);
+                    await SynthesizeSpeechByKoeiromapAsync(cancellationToken);
                     break;
 
                 default:
@@ -75,7 +157,7 @@ namespace Mochineko.SpeechSynthesis
             }
         }
 
-        private async UniTask SynthesisSpeechByVoicevoxAsync(CancellationToken cancellationToken)
+        private async UniTask SynthesizeSpeechByVoicevoxAsync(CancellationToken cancellationToken)
         {
             await UniTask.SwitchToThreadPool();
 
@@ -141,10 +223,10 @@ namespace Mochineko.SpeechSynthesis
                     throw new UncertainResultPatternMatchException(nameof(synthesizeResult));
             }
 
-            await DecodeAndPlayAsync(stream, cancellationToken);
+            await PlayAsync(stream, cancellationToken);
         }
 
-        private async UniTask SynthesisSpeechByKoeiromapAsync(CancellationToken cancellationToken)
+        private async UniTask SynthesizeSpeechByKoeiromapAsync(CancellationToken cancellationToken)
         {
             await UniTask.SwitchToThreadPool();
 
@@ -170,7 +252,7 @@ namespace Mochineko.SpeechSynthesis
                     Debug.LogError(
                         $"[SpeechSynthesis] Failed to synthesis speech bu Koeiromap because -> {synthesisRetryable.Message}.");
                     return;
-                
+
                 case IUncertainFailureResult<SpeechSynthesisResult> synthesisFailure:
                     Debug.LogError(
                         $"[SpeechSynthesis] Failed to synthesis speech bu Koeiromap because -> {synthesisFailure.Message}.");
@@ -179,11 +261,28 @@ namespace Mochineko.SpeechSynthesis
                 default:
                     throw new UncertainResultPatternMatchException(nameof(synthesisResult));
             }
-            
-            await DecodeAndPlayAsync(stream, cancellationToken);
+
+            await PlayAsync(stream, cancellationToken);
         }
 
-        private async UniTask DecodeAndPlayAsync(Stream stream, CancellationToken cancellationToken)
+        private async UniTask PlayAsync(Stream stream, CancellationToken cancellationToken)
+        {
+            switch (playerBackend)
+            {
+                case PlayerBackend.AudioSource:
+                    await PlayByAudioSourceAsync(stream, cancellationToken);
+                    break;
+
+                case PlayerBackend.DirectSound:
+                    await PlayByDirectSoundAsync(stream, cancellationToken);
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(playerBackend));
+            }
+        }
+
+        private async UniTask PlayByAudioSourceAsync(Stream stream, CancellationToken cancellationToken)
         {
             if (audioSource == null)
             {
@@ -212,7 +311,7 @@ namespace Mochineko.SpeechSynthesis
             }
             catch (Exception e)
             {
-                Debug.LogException(e);
+                Debug.LogError($"[SpeechSynthesis] Failed to decode audio because -> {e}.");
                 return;
             }
             finally
@@ -220,9 +319,19 @@ namespace Mochineko.SpeechSynthesis
                 await stream.DisposeAsync();
             }
 
-            await UniTask.SwitchToMainThread(cancellationToken);
-
             await audioSource.PlayAsync(audioClip, cancellationToken);
+        }
+
+        private async UniTask PlayByDirectSoundAsync(Stream stream, CancellationToken cancellationToken)
+        {
+            directSoundDevice?.Dispose();
+            
+            directSoundDevice = new DirectSoundOut(device: directSoundDevices[directSoundDeviceID].Guid);
+            var reader = new WaveFileReader(stream);
+
+            directSoundDevice.Init(reader);
+
+            await directSoundDevice.PlayAsync(cancellationToken);
         }
     }
 }
---
+++ Assets/Mochineko/SpeechSynthesis/DirectSoundOutExtension.cs
@@ -0,0 +1,33 @@
+#nullable enable
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Mochineko.SpeechSynthesis
+{
+    internal static class DirectSoundOutExtension
+    {
+        public static async UniTask PlayAsync(
+            this DirectSoundOut device,
+            CancellationToken cancellationToken)
+        {
+            device.Stop();
+
+            device.Play();
+
+            try
+            {
+                await UniTask.WaitUntil(
+                    () => device.PlaybackState == PlaybackState.Stopped,
+                    cancellationToken: cancellationToken);
+                
+                device.Stop();
+            }
+            catch (OperationCanceledException exception)
+            {
+                device.Stop();
+            }
+        }
+    }
+}
